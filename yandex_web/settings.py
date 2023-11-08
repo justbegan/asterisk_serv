937f406c91bc1b5f8ea3d05ab223449e0e87fffc
justbegan <justbegan@mail.ru>
2023-11-08 15:58:28 +0900

    fix allowed hosts

--- yandex_web/settings.py
+++ yandex_web/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 INSTALLED_APPS = [
     'django.contrib.admin',
